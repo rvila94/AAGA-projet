373b5c705712803e97c944fd79f6cf7f33aabc41
VILA Rodrigo <r94vila@gmail.com>
2025-10-10 17:13:41 +0200

    Ajout fonctions simples pour graphe

---
+++ src/graph_utils.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+
+def generate_graph(n=10, m=15):
+    """Generates a random graph with n nodes and m edges"""
+    G = nx.gnm_random_graph(n, m)
+    return G
+
+def plot_graph(G, title="Graoh"):
+    """Plot a given graph"""
+    nx.draw(G, with_labels=True, node_color='blue', edge_color='gray')
+    plt.title(title)
+    plt.show()
+    
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from graph_utils import generate_graph, plot_graph
+
+def main():
+
+    G = generate_graph()
+    plot_graph(G)
+
+if __name__ == "__main__":
+    main()
