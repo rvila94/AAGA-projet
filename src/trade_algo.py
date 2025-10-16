3447425e4320cc7456219b83306b7280713e2fff
URGaruda <dabakonedaba@gmail.com>
2025-10-16 20:54:40 +0200

    ajout d'une iteration algo trade

---
+++ src/trade_algo.py
@@ -0,0 +1,79 @@
+import random
+
+
+matrice_essai = [[1,0,1,1,0],
+                 [0,1,0,1,0],
+                 [1,0,0,1,1],
+                 [1,1,0,0,0]]
+
+# Fonction qui génère 2 entier aléatoire différents entre 0 et n-1 (afin de pouvoir choisir 2 lignes aléatoire de la matrice pour le trade)
+def generate_2(n):
+    x=random.randint(0,n-1)
+    y=random.randint(0,n-1)
+    while y==x: 
+        y=random.randint(0,n-1)
+
+    return x,y
+
+# Fonction qui va retourner les listes des index des éléments des 2 listes sur lequels ils n'ont pas de d'occurences de 1 en commum 
+# + la liste des index pour lequel ils ont un élément en commum
+# On suppose que les 2 listes ont la même taille 
+def no_equal(l1,l2): 
+    res1=[]
+    res2=[]
+    taille=len(l1)
+    for i in range(0,taille):
+        if l1[i]==1:
+            if l2[i]!=1:
+                res1.append(i)
+        if l2[i]==1:
+            if l1[i]!=1:
+                res2.append(i)
+
+    print("l1 vaut "+str(l1))
+    print("l2 vaut "+str(l2))
+    print("--------")
+    print("res1 vaut "+str(res1))
+    print("res2 vaut "+str(res2))
+    return res1,res2
+
+
+# Algo des trades ou curveball dans la doc
+#On suppose que M contient au moins une liste pour son premier élément
+def curveball(M):
+    res=M
+    taille_ligne=len(res)
+    #Etape 1 : obtenir 2 listes aléatoires dans la matrice 
+    i_1,i_2=generate_2(taille_ligne)
+    print("index de l1 = "+str(i_1))
+    print("index de l2 = "+str(i_2))
+    l1=M[i_1]
+    l2=M[i_2]
+    #Etape 2 : on prive de ces 2 listes les occurrences d'index qui donnent tout les 2 un élément différent de 0 dans les listes
+    L1,L2=no_equal(l1,l2)
+    #Etape 3 : on échange un élément d'une des listes obtenus dans l'étape 2 
+    x=random.randint(0,len(L1)-1)
+    y=random.randint(0,len(L2)-1)
+    print("valeur de L1 qui va être échangé : "+str(L1[x]))
+    print("valeur de L2 qui va être échangé : "+str(L2[y]))
+    del1=L1[x] #ancienne valeur qui va devoir être mis à 0 lors de la mise à jour de la liste 
+    del2=L2[y] #same
+    #échange
+    tmp=L2[y]
+    L2[y]=L1[x]
+    L1[x]=tmp
+    #changement sur la matrice principal
+    l1[del1]=0
+    l2[del2]=0
+    l1[L1[x]]=1
+    l2[L2[y]]=1
+    res[i_1]=l1
+    res[i_2]=l2
+    return res 
+
+print(matrice_essai)
+print("----------")
+new_m=curveball(matrice_essai)
+print(new_m)
+
+
