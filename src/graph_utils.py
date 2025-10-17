5d8be968139cbee9f4a8ff5674eb6032b7c6da51
Martin Poncet <poncetmartin2003@gmail.com>
2025-10-17 17:54:54 +0200

    Ajout d'une premiere version de test de trade algo

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -2,15 +2,24 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
-def generate_graph(n=10, m=15):
+def generate_graph(n=5, m=7):
     """Generates a random graph with n nodes and m edges"""
-    G = nx.gnm_random_graph(n, m)
+    # complete_bipartite_graph(n1, n2[, create_using]) Returns the complete bipartite graph K_{n_1,n_2}.
+    G = nx.gnm_random_graph(n, m, directed=True)
     return G
 
-def plot_graph(G, title="Graoh"):
+def plot_graph(G, new_G, title="Graph"):
     """Plot a given graph"""
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
     nx.draw(G, with_labels=True, node_color='blue', edge_color='gray')
     plt.title(title)
+
+    plt.subplot(1, 2, 2)
+    nx.draw(new_G, with_labels=True, node_color='red', edge_color='gray')
+    plt.title("Modified " + title)
+
     plt.show()
     
 def degree_sequence(G):
--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
 from graph_utils import generate_graph, plot_graph, degree_sequence
 from swap_algo import swap_randomization
 from convergence import run_empirical, run_until_stable
+import networkx as nx
+
+from trade_algo import curveball
 
 def main():
 
     G = generate_graph()
+    M = nx.to_numpy_array(G)
     #plot_graph(G)
 
-    new_G = run_empirical(G, swap_randomization, verbose=True)
-    #plot_graph(new_G)
+    new_G_swap = run_empirical(G, swap_randomization, verbose=True)
+    print("Matrice avant trade :")
+    print(M)
+    adj_matrix_trade = curveball(M)
+    print("Matrice après trade :")
+    print(adj_matrix_trade)
+    new_G_trade = nx.from_numpy_array(adj_matrix_trade, create_using=nx.DiGraph)
+
+    plot_graph(G, new_G_swap)
+    plot_graph(G, new_G_trade)
 
     deg_before = degree_sequence(G)
-    deg_after  = degree_sequence(new_G)
+    deg_after  = degree_sequence(new_G_swap)
     assert sorted(deg_before) == sorted(deg_after), "Erreur: Séquence de degrés modifiée !"
 
 if __name__ == "__main__":
