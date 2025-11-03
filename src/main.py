d8a0e62b7b5e0131ae527693ef1d1d1d6bcbec61
URGaruda <dabakonedaba@gmail.com>
2025-11-03 23:56:55 +0100

    Ajout d'une comparaison entre swap et trades

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from swap_algo import swap_randomization
 from trade_algo import curveball, undirected_curveball
 from convergence import run_empirical, run_until_stable
 import networkx as nx
+import numpy as np
+import time
 
 
 def test_randomization_convergence(
@@ -61,11 +63,58 @@ def test_randomization_convergence(
 
 def main():
     # Générer un graphe simple non orienté
-    #G = nx.erdos_renyi_graph(10, 0.8, seed=42)
-    G = generate_graph(5,7)
+    n=100
+    m=500
+    G = generate_graph(n, m)
 
-    # Lancer le test sur l'algorithme de swap
-    test_randomization_convergence(G, undirected_curveball)
+    A = nx.to_numpy_array(G)
+
+    #np.savetxt("matrice_adjacence1.txt", A, delimiter=",", fmt="%d") #Pour générer un fichier avec la matrice du graphe 
+
+    # Liste des algorithmes à comparer
+    algorithms = [undirected_curveball,swap_randomization]
+
+    results = []
+
+    for algo in algorithms:
+        start_time = time.time()
+        new_G, history, n_trials = test_randomization_convergence(G, algo, verbose=False)
+        end_time = time.time()
+
+        ls_triangle = history["triangles"]
+        ls_clust = history["avg_clustering"]
+        ls_comp = history["largest_component"]
+
+        """print("aaaaaaaa")
+        print(history["triangles"])
+        print("bbbbbbbbbb")"""
+
+        algo_name = ""
+        if algo.__name__ == "swap_randomization" :
+            algo_name = "Swap"
+        else:
+            algo_name ="Trades"
+        # Enregistrer les résultats
+        results.append({
+            "algorithm": algo_name,
+            "execution_time": end_time - start_time,
+            "num_triangles": ls_triangle[-1],
+            "average_clustering":ls_clust[-1],
+            "largest_connected_component" : ls_comp[-1],
+            "convergence_iterations": n_trials
+        })
+
+    # Afficher les résultats
+    print(f"\n=== Comparaison des algorithmes pour un graphe avec {n} noeuds et {m} arrêtes ===")
+    for result in results:
+        print(f"Algorithme: {result['algorithm']}")
+        print(f"  Temps d'exécution: {result['execution_time']:.4f} s")
+        print(f"  Nombre de triangles: {result['num_triangles']}")
+        print(f"  Coefficient de clustering: {result['average_clustering']}")
+        print(f"  Taille du plus grand composant connexe: {result['largest_connected_component']}")
+        print(f"  Itérations de convergence: {result['convergence_iterations']}")
+        
+        
 
 if __name__ == "__main__":
     main()
